a886b0f2e5e481f86ea2b3cf5307bbc4dcb6c099
Victor Braga <victorbraga92@gmail.com>
2016-06-07 16:36:54 -0300

    SomaPixels Sequencial
    Módulo SomaPixels agora é sequencial para diminuir número de elementos
    lógicos.

--- v/ProcessamentoDigito.v
+++ v/ProcessamentoDigito.v
@@ -18,16 +18,16 @@ module ProcessamentoDigito(
 	Diferenca8 diferenca8(numero,diff_pixel[8]);
 	Diferenca9 diferenca9(numero,diff_pixel[9]);
 	
-	SomaPixels somaPixels0(diff_pixel[0],v_diferenca[0]);
-	SomaPixels somaPixels1(diff_pixel[1],v_diferenca[1]);
-	SomaPixels somaPixels2(diff_pixel[2],v_diferenca[2]);
-	SomaPixels somaPixels3(diff_pixel[3],v_diferenca[3]);
-	SomaPixels somaPixels4(diff_pixel[4],v_diferenca[4]);
-	SomaPixels somaPixels5(diff_pixel[5],v_diferenca[5]);
-	SomaPixels somaPixels6(diff_pixel[6],v_diferenca[6]);
-	SomaPixels somaPixels7(diff_pixel[7],v_diferenca[7]);
-	SomaPixels somaPixels8(diff_pixel[8],v_diferenca[8]);
-	SomaPixels somaPixels9(diff_pixel[9],v_diferenca[9]);
+	SomaPixels somaPixels0(diff_pixel[0],flag,v_diferenca[0]);
+	SomaPixels somaPixels1(diff_pixel[1],flag,v_diferenca[1]);
+	SomaPixels somaPixels2(diff_pixel[2],flag,v_diferenca[2]);
+	SomaPixels somaPixels3(diff_pixel[3],flag,v_diferenca[3]);
+	SomaPixels somaPixels4(diff_pixel[4],flag,v_diferenca[4]);
+	SomaPixels somaPixels5(diff_pixel[5],flag,v_diferenca[5]);
+	SomaPixels somaPixels6(diff_pixel[6],flag,v_diferenca[6]);
+	SomaPixels somaPixels7(diff_pixel[7],flag,v_diferenca[7]);
+	SomaPixels somaPixels8(diff_pixel[8],flag,v_diferenca[8]);
+	SomaPixels somaPixels9(diff_pixel[9],flag,v_diferenca[9]);
 	
 	MenorDistancia menorDistancia(v_diferenca, flag, digito);
 	
